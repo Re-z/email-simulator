37fb479d05ca5f87a65f6e8a5ec7a52bb57076eb
Re-z <re-z@ukr.net>
2019-01-13 20:39:04 +0200

    checking fields done

--- js/app.js
+++ js/app.js
@@ -5,23 +5,25 @@ let form = document.getElementById('email-form'),
         message = document.getElementById('message'),
         resetBtn = document.getElementById('resetBtn'),
         successFieldStyles = 'box-shadow: 0 1px 0 0 #26a69a; border-bottom: 1px solid #26a69a;',
-        errorFieldStyles = 'box-shadow: 0 1px 0 0 red; border-bottom: 1px solid red;';
+        errorFieldStyles = 'box-shadow: 0 1px 0 0 red; border-bottom: 1px solid red;',
+        defaultFieldStyles = 'box-shadow: none; border-bottom: 1px solid grey';
 
 //functions
 function checkEmailField(ev){
     let emailValue = email.value;
-    if(emailValue.indexOf('@') != -1) {email.style.cssText = successFieldStyles;} 
-    else {email.style.cssText = errorFieldStyles}
+    if(emailValue.indexOf('@') != -1) {email.style.cssText = successFieldStyles; return true;} 
+    else {email.style.cssText = errorFieldStyles; return false}
     
 }
 function checkSubjectField(evt) {
-    if(subject.value) {subject.style.cssText = successFieldStyles;} 
-    else {subject.style.cssText = errorFieldStyles}
+    // console.log(typeof subject.value)
+    if(subject.value.length) {subject.style.cssText = successFieldStyles; return true;} 
+    else {subject.style.cssText = errorFieldStyles; return false}
 }
 
 function checkMessageField(evt) {
-    if(message.value) {message.style.cssText = successFieldStyles} 
-    else {message.style.cssText = errorFieldStyles}
+    if(message.value.length) {message.style.cssText = successFieldStyles; return true;} 
+    else {message.style.cssText = errorFieldStyles;return false;}
 }
 
 
@@ -30,6 +32,10 @@ function checkMessageField(evt) {
 
 function handleForm(evt) {
         evt.preventDefault();
+        checkEmailField();
+        checkSubjectField();
+        checkMessageField();
+        
         if(checkEmailField() && checkSubjectField() && checkMessageField()) {
             alert(1)
         }
@@ -40,6 +46,13 @@ email.addEventListener('blur', checkEmailField);
 subject.addEventListener('blur', checkSubjectField);
 message.addEventListener('blur', checkMessageField);
 form.addEventListener('submit', handleForm);
-resetBtn.addEventListener('click', function(){form.reset()});
+
+resetBtn.addEventListener('click', function(){
+    // form.reset();
+    email.style.cssText = defaultFieldStyles;
+    subject.style.cssText = defaultFieldStyles;
+    message.style.cssText = defaultFieldStyles;
+});
+
 
 
